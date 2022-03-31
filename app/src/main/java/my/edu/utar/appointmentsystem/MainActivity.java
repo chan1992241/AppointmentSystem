6be8e9f8e9aac51389ff088be285164b33fc5016
chan1992241 <chan1992241@gmail.com>
2022-03-31 21:34:46 +0800

    Create Simple Splash Screen

--- app/src/main/java/my/edu/utar/appointmentsystem/MainActivity.java
+++ app/src/main/java/my/edu/utar/appointmentsystem/MainActivity.java
@@ -2,13 +2,45 @@ package my.edu.utar.appointmentsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_role_selection);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        Animation topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        Animation bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        ImageView appImage = findViewById(R.id.appImage);
+        TextView appName = findViewById(R.id.appName);
+        TextView appDesc = findViewById(R.id.appDescription);
+
+        appImage.setAnimation(topAnimation);
+        appName.setAnimation(bottomAnimation);
+        appDesc.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, roleSelection.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+
     }
 }
